5a750f40b6f57775418bb9b020ea8eb7886f6241
Snith SHibu <snithshibu.b23ec1156@mbcet.ac.in>
2024-12-17 19:46:04 +0300

    changes

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,7 +1,18 @@
-// helper functions
-import { calculateSpentByBudget, formatCurrency, formatPercentage } from "../helpers";
+// rrd imports
+import { Form, Link } from "react-router-dom";
 
-const BudgetItem = ({ budget }) => {
+//library imports
+import { BanknotesIcon } from "@heroicons/react/24/outline";
+
+//helper functions
+import { 
+    calculateSpentByBudget, 
+    formatCurrency, 
+    formatPercentage 
+} from "../helpers";
+import { TrashIcon } from "@heroicons/react/20/solid";
+
+const BudgetItem = ({ budget, showDelete = false }) => {
     const { id, name, amount, color } = budget;
     const spent = calculateSpentByBudget(id);
 
@@ -26,6 +37,39 @@ const BudgetItem = ({ budget }) => {
                 <small>{formatCurrency(spent)} spent</small>
                 <small>{formatCurrency(amount - spent)} remaining</small>
             </div>
+            {
+                showDelete ? (
+                    <div className="flex-sm">
+                        <Form
+                            method="post"
+                            action="delete"
+                            onSubmit={(event) => {
+                                if(!confirm("Are you sure you want to permanently delete this budget?")){
+                                    event.preventDefault();
+                                }
+                            }}
+                        >
+                            <button
+                                typ="submit"
+                                className="btn"
+                            >
+                                <span>Delete Budget</span>
+                                <TrashIcon width={20} />
+                            </button>
+                        </Form>
+                    </div>
+                ) : (
+                    <div className="flex-sm">
+                        <Link
+                            to={`/budget/${id}`}
+                            className="btn"
+                        >
+                            <span>View Details</span>
+                            <BanknotesIcon width={20} />
+                        </Link>
+                    </div>
+                )
+            }
         </div>
     )
 }
