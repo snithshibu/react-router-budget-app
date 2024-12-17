c2daebfcfbf1aa4d9abbd589fdaead705d51e968
Snith SHibu <snithshibu.b23ec1156@mbcet.ac.in>
2024-12-17 15:34:57 +0300

    changes

---
+++ src/helpers.js
@@ -0,0 +1,81 @@
+export const waait = () => new Promise((res) => setTimeout(res, Math.random() * 800));
+  
+// colors
+const generateRandomColor = () => {
+    const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+    return `${existingBudgetLength * 34} 65% 50%`;
+};
+  
+// Local storage
+export const fetchData = (key) => {
+    return JSON.parse(localStorage.getItem(key));
+};
+  
+// create budget
+export const createBudget = ({ name, amount }) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        color: generateRandomColor(),
+    };
+    const existingBudgets = fetchData("budgets") ?? [];
+    return localStorage.setItem(
+        "budgets",
+        JSON.stringify([...existingBudgets, newItem])
+    );
+};
+  
+// create expense
+export const createExpense = ({ name, amount, budgetId }) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        budgetId: budgetId,
+    };
+    const existingExpenses = fetchData("expenses") ?? [];
+    return localStorage.setItem(
+        "expenses",
+        JSON.stringify([...existingExpenses, newItem])
+    );
+};
+  
+// delete item
+export const deleteItem = ({ key }) => {
+    return localStorage.removeItem(key);
+};
+  
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+    const expenses = fetchData("expenses") ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        // check if expense.id === budgetId I passed in
+        if (expense.budgetId !== budgetId) return acc;
+        
+        // add the current amount to my total
+        return (acc += expense.amount);
+    }, 0);
+    return budgetSpent;
+};
+  
+// FORMATTING
+export const formatDateToLocaleString = (epoch) => new Date(epoch).toLocaleDateString();
+  
+// Formating percentages
+export const formatPercentage = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "percent",
+        minimumFractionDigits: 0,
+    });
+};
+  
+// Format currency
+export const formatCurrency = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD",
+    });
+};
